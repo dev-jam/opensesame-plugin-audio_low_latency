5ade98e7cda6e2821d728524e14c5bf2906ef4c8
dev-jam <mail@bobrosbag.nl>
2023-12-04 13:52:32 +0100

    bump version

--- opensesame_plugins/audio_low_latency/audio_low_latency_play_start/__init__.py
+++ opensesame_plugins/audio_low_latency/audio_low_latency_play_start/__init__.py
@@ -48,6 +48,6 @@ controls = [
         "label": "<small><b>Note:</b> Audio Low Latency Play Init item at the begin of the experiment is needed for initialization of the audio device</small>"
     }, {
         "type": "text",
-        "label": "<small>Audio Low Latency version 10.2.0</small>"
+        "label": "<small>Audio Low Latency version 10.3.0</small>"
     }
 ]
